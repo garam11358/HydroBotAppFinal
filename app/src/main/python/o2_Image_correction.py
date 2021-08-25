c547d9f325b644aa6f3d40c878b74dbdc816778f
garam11358 <62483993+garam11358@users.noreply.github.com>
2021-08-24 23:23:37 -0400

    Update o2_Image_correction.py

--- app/src/main/python/o2_Image_correction.py
+++ app/src/main/python/o2_Image_correction.py
@@ -1,13 +1,13 @@
-import opencv
+import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 
 def o2_Image_correction(title, threshold, newPicPath):
 ## 이미지 불러들이기 ##
-    img_tr = opencv.imread(title)
-#opencv.imshow('Tr_Image', img_tr)
-#opencv.waitKey(0)
+    img_tr = cv2.imread(title)
+#cv2.imshow('Tr_Image', img_tr)
+#cv2.waitKey(0)
 
 ## 이미지 히스토그램 확인 ##
     plt.hist(img_tr.ravel(), 256, [0,256]); 
@@ -17,11 +17,11 @@ def o2_Image_correction(title, threshold, newPicPath):
 ## adaptive threshold 적용으로 개선 가능
 
     threshold_num = threshold
-#ret,thresh1 = opencv.threshold(img_tr,threshold_num,255,opencv.THRESH_BINARY)
-    ret,thresh2 = opencv.threshold(img_tr,threshold_num,255,opencv.THRESH_BINARY_INV)
-#ret,thresh3 = opencv.threshold(img_tr,threshold_num,255,opencv.THRESH_TRUNC)
-#ret,thresh4 = opencv.threshold(img_tr,threshold_num,255,opencv.THRESH_TOZERO)
-#ret,thresh5 = opencv.threshold(img_tr,threshold_num,255,opencv.THRESH_TOZERO_INV)
+#ret,thresh1 = cv2.threshold(img_tr,threshold_num,255,cv2.THRESH_BINARY)
+    ret,thresh2 = cv2.threshold(img_tr,threshold_num,255,cv2.THRESH_BINARY_INV)
+#ret,thresh3 = cv2.threshold(img_tr,threshold_num,255,cv2.THRESH_TRUNC)
+#ret,thresh4 = cv2.threshold(img_tr,threshold_num,255,cv2.THRESH_TOZERO)
+#ret,thresh5 = cv2.threshold(img_tr,threshold_num,255,cv2.THRESH_TOZERO_INV)
 
 #plt.subplot(231),plt.imshow(img_tr),plt.title('Original')
 #plt.subplot(232),plt.imshow(thresh1),plt.title('THRESH_BINARY')
@@ -42,10 +42,10 @@ def o2_Image_correction(title, threshold, newPicPath):
 
     kernel = np.ones((5,5),np.uint8) #객체 사이즈에 따라 변경
 
-#img_tr_th_er = opencv.erode(img_tr_th,kernel,iterations = 1) # erosion
-#img_tr_th_di = opencv.dilate(img_tr_th,kernel,iterations = 1) # dilation
-#img_tr_th_op = opencv.morphologyEx(img_tr_th, opencv.MORPH_OPEN, kernel) # opening
-    img_tr_th_cl = opencv.morphologyEx(img_tr_th, opencv.MORPH_CLOSE, kernel) # closing
+#img_tr_th_er = cv2.erode(img_tr_th,kernel,iterations = 1) # erosion
+#img_tr_th_di = cv2.dilate(img_tr_th,kernel,iterations = 1) # dilation
+#img_tr_th_op = cv2.morphologyEx(img_tr_th, cv2.MORPH_OPEN, kernel) # opening
+    img_tr_th_cl = cv2.morphologyEx(img_tr_th, cv2.MORPH_CLOSE, kernel) # closing
 
 #plt.subplot(231),plt.imshow(img_tr_th),plt.title('Original')
 #plt.subplot(232),plt.imshow(img_tr_th_er),plt.title('erosion')
@@ -66,11 +66,6 @@ def o2_Image_correction(title, threshold, newPicPath):
     plt.show()
 
 ## 결과 저장 ##
-    opencv.imshow('img_final', img_final)
-    opencv.waitKey(0)
-    opencv.imwrite(newPicPath,img_final)
-
-
-
-
-
+    cv2.imshow('img_final', img_final)
+    cv2.waitKey(0)
+    cv2.imwrite(newPicPath,img_final)
